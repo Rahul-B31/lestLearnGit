e71c429a7bfa1f1a2afd20fec9aed95989205b9a
rahul barhate <rahulbarhate444@gmail.com>
2022-07-31 21:26:32 +0530

    adding multiply

--- learnGit.java
+++ learnGit.java
@@ -57,7 +57,7 @@ Now
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("sum is "+(a-b));
+        System.out.println("Multiplication  is "+(a*b));
     }
 
 }
