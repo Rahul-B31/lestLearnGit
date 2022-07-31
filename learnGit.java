28e2be5b7fb4bc2d6ea0ad26412e1927da69e43e
rahul barhate <rahulbarhate444@gmail.com>
2022-07-31 20:49:19 +0530

    intial commit

---
+++ learnGit.java
@@ -0,0 +1,45 @@
+public class git {
+    
+// what is git ?
+//ans: git is version control system
+// It save all changes of our code 
+
+/*  step 1: add the config
+  using the command 
+  git config --global user.name "your name"
+
+  Add the email
+  git config --global user.email "your email"
+*******************************
+
+ for change in direct config file 
+
+ cmd - git config --global --edit
+
+// for Exit the file ->
+cmd  esc ":wq"
+
+
+// making git repo
+
+cmd "git init"
+
+
+// for checking the all status of the file 
+
+cmd git status 
+
+// for adding the file 
+
+cmd git add <file name> Now, the file is in the 
+ staging area
+
+
+*/  
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("sum is "+(a+b));
+    }
+
+}
